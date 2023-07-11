c44608c625fa636934b3a158bcbac077e658526c
irlandets1980 <irlandets1980@gmail.com>
2023-07-11 20:21:37 +0300

    serv1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const text = require('./functions/standart_functions')
 const HTTPrequest1C = require('./functions/function_1c');
 
 const token = '5702490586:AAFuAJTmWEDhxyz7aZxfsNNq-l-lzAjoSpc';
-const webAppUrl = 'https://illustrious-mandazi-76fa81.netlify.app/#/';
+const webAppUrl = 'http://illustrious-mandazi-76fa81.netlify.app/#/';
 
 const bot = new TelegramBot(token, {
     polling: true
